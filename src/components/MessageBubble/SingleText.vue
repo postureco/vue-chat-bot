42496d5254fc42e263ab62a0c93ef71ff3b624da
Jason Kobilka <jkobilka@gmail.com>
2022-03-24 11:54:49 -0600

    adding activeMsgAction slot for html messages

--- src/components/MessageBubble/SingleText.vue
+++ src/components/MessageBubble/SingleText.vue
@@ -6,7 +6,12 @@
     :class="{ 'has-msg-action' : hasMsgAction && isLatest && isBot}"
     ) {{ mainData.text }}
     slot(name="activeMsgAction" v-if="isLatest && isBot")
-  .qkb-msg-bubble-component__text(v-if="['html', 'button'].includes(mainData.type)" v-html="mainData.text")
+  .qkb-msg-bubble-component__text(
+      v-if="['html', 'button'].includes(mainData.type)"
+      :class="{ 'has-msg-action' : hasMsgAction && isLatest && isBot}"
+      v-html="mainData.text")
+      slot(name="activeMsgAction" v-if="isLatest && isBot")
+
 </template>
 <script>
 export default {
